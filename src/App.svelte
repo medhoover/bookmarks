f6f40de1f374aac32826b039619cc2bd8f98fbba
Mouhamed GHARSALLAH <gmedhoover@gmail.com>
2021-12-12 13:45:57 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Hello User!</h1>
+
+  <p>
+    This project is getting made at the moment.
+  </p>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
