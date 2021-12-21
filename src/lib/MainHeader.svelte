b11bb68f2287bf89100c724a58636fe898ecd2d9
ghar <mohamed.gharsallah.tn@gmail.com>
2021-12-21 23:55:09 +0100

    Show my bookmarks link when user is logged in

--- src/lib/MainHeader.svelte
+++ src/lib/MainHeader.svelte
@@ -17,7 +17,9 @@
   </Link>
 
   <div class="flex hidden md:block flex-row xxl:space-x-24 xl:space-x-16 lg:space-x-8 space-x-4">
-    <Link to="/profile">My Spacemarks</Link>
+    {#if isUserLoggedIn}
+      <Link to="/profile">My Spacemarks</Link>
+    {/if}
     <Link to="/spacemarks/how_it_works">How it works</Link>
     <a class="font-semibold underline-offset-8 hover:underline" href="https://github.com/spacemarks" target="_blank"
       >Open source</a>
